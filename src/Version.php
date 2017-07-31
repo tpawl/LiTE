90e932854e76050795083572b156de70b6174ba1
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-07-31 23:38:46 +0200

    Create Version.php

---
+++ src/Version.php
@@ -0,0 +1,18 @@
+<?php
+// Copyright (c) 2013 - 2017 by Thomas Pawlitschko. All rights reserved.
+
+declare(strict_types=1);
+
+namespace LiTE;
+
+class Version
+{
+    // Major version number (major release)
+    public const MAJOR = 1;
+
+    // Minor version number (minor release)
+    public const MINOR = 0;
+
+    // Revision number (patch level)
+    public const REVISION = 0;
+}
