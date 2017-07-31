12568975c983fe6b3db1d2e14a494397178bed90
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-07-31 23:28:42 +0200

    Create AssertTest.php

---
+++ test/AssertTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LiTE\Tests;
+
+use PHPUnit\Framework\TestCase;
+use LiTE\Assert\Assert;
+
+class AssertTest extends TestCase
+{
+    /**
+     * @expectedException Felix\Packages\LiTE\Exceptions\AssertException
+     */
+    public function testFalseConditionThrowsAnException()
+    {   
+        Assert::isTrue(false);
+    }
+    
+    public function testTrueConditionDoesNothing()
+    {
+        Assert::isTrue(true);
+    }
+}
