bee15edad58728b819615766ff6f0a5158fcd607
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-08-02 00:25:25 +0200

    Create TemplateExpression.php

---
+++ src/Expressions/TemplateExpression.php
@@ -0,0 +1,71 @@
+<?php
+// Copyright (c) 2013 - 2017 by Thomas Pawlitschko. All rights reserved.
+
+declare(strict_types=1);
+
+namespace LiTE\Expressions;
+
+use LiTE\ErrorHandlers\ErrorHandlers;
+use LiTE\Context\Context;
+use LiTE\Context\ViewHelperContext;
+
+class TemplateExpression extends SubTemplateExpression
+{
+    /**
+     * @var string
+     */
+    private $viewHelperDirectory;
+
+    /**
+     * @var string
+     */
+    private $viewHelperNamespace;
+
+    /**
+     * @var callable|null;
+     */
+    private $viewHelperErrorHandler;
+
+    /**
+     * @param string $viewHelperDirectory
+     * @param string $viewHelperNamespace
+     * @return void
+     */
+    public function __construct(string $template, array $variables, string $viewHelperDirectory, string $viewHelperNamespace)
+    {
+        parent::__construct($template, $variables);
+        $this->viewHelperDirectory = realpath($viewHelperDirectory);
+        $this->viewHelperNamespace = $viewHelperNamespace;
+        $this->viewHelperErrorHandler = ErrorHandlers::top();
+    }
+
+    /**
+     * @param string $template
+     * @param array $values
+     * @return void
+     */
+    protected function initialize(string $template, array $values): void
+    {
+        parent::initialize($template, $values);
+
+        $context = Context::getInstance();
+
+        $viewHelperContext = new ViewHelperContext($this->viewHelperDirectory, $this->viewHelperNamespace, $this->viewHelperErrorHandler);
+
+        $context->setViewHelperContext($viewHelperContext);
+    }
+
+    /**
+     * @return void
+     */
+    protected function cleanup(): void
+    {
+        $context = Context::getInstance();
+
+        $context->clearViewHelperContext();
+
+        parent::cleanup();
+
+        $context->clearTemplateContext();
+    }
+}
