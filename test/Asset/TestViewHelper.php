2a0120af9676eef6962b2ae01975753387a355a3
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-08-01 17:33:34 +0200

    Update TestViewHelper.php

--- test/Asset/TestViewHelper.php
+++ test/Asset/TestViewHelper.php
@@ -1,1 +1,12 @@
+<?php
+// Copyright (c) 2017 by Thomas Pawlitschko. All rights reserved.
 
+use LiTE\ViewHelperInterface;
+
+class TestViewHelper implements ViewHelperInterface
+{
+    public static function execute(array $arguments): void
+    {
+        print 'Test';
+    }
+}
