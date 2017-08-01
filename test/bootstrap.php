c4c8ed3bce8665fd9ca80ea606c5a076146be991
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-08-01 14:59:03 +0200

    Update bootstrap.php

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,11 +1,9 @@
 <?php
-// Copyright (c) 2017 by Thomasd Pawlitschko. All rights reserved.
+// Copyright (c) 2017 by Thomas Pawlitschko. All rights reserved.
 
 function loader($class)
 {
     $file = __DIR__ . '/../src/' . str_replace('\\', '/', substr($class, 5)) . '.php';
-    
-    var_dump($file);
     
     if (file_exists($file)) {
         require $file;
