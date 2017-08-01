95a27054d3187366980328957798b01609327ad3
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-08-01 20:41:34 +0200

    Create TemplateContextTest.php

---
+++ test/TemplateContextTest.php
@@ -0,0 +1,12 @@
+<?php
+// Copyright (c) 2017 by Thomas Pawlitschko. All rights reserved.
+
+declare(strict_types=1);
+
+namespace LiTE\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class TemplateContextTest extends TestCase
+{
+}
