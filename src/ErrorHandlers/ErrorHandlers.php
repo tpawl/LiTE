f6b8918122236d083030935d803a495622246f16
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-08-01 17:40:38 +0200

    Create ErrorHandlers.php

---
+++ src/ErrorHandlers/ErrorHandlers.php
@@ -0,0 +1,38 @@
+<?php
+// Copyright (c) 2015 - 2017 by Thomas Pawlitschko. All rights reserved.
+
+declare(strict_types=1);
+
+namespace LiTE\ErrorHandlers;
+
+class ErrorHandlers
+{
+    /**
+     * @param callable|null $errorHandler
+     * @return void
+     */
+    public static function push(?callable $errorHandler): void
+    {
+        set_error_handler($errorHandler);
+    }
+
+    /**
+     * @return callable|null
+     */
+    public static function top(): ?callable
+    {
+        $previousErrorHandler = set_error_handler(null);
+
+        self::pop();
+
+        return $previousErrorHandler;
+    }
+
+    /**
+     * @return void
+     */
+    public static function pop(): void
+    {
+        restore_error_handler();
+    }
+}
