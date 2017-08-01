3270eeaa6d1b8a9629719372149393f51880c562
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-08-02 00:58:27 +0200

    Create SubTemplateExpressionTest.php

---
+++ test/SubTemplateExpressionTest.php
@@ -0,0 +1,29 @@
+<?php
+// Copyright (c) 2017 by Thomas Pawlitschko. All rights reserved.
+
+declare(strict_types=1);
+
+namespace LiTE\Tests;
+
+use PHPUnit\Framework\TestCase;
+use LiTE\Context\Context;
+use LiTE\Expressions\TemplateExpression;
+use LiTE\Expressions\SubTemplateExpression;
+
+class SubTemplateExpressionTest extends TestCase
+{
+    public function testDisplay()
+    {
+        $te = new TemplateExpression('', [], __DIR__ . '/Asset', '');
+        
+        $ste = new SubTemplateExpression('abc<?php $this->var; ?><?php self::test(); ?>', ['var' => 'def']);
+        
+        $this->expectOutputString('abcdefabc');
+        
+        $ste->display();
+        
+        $instance = Context::getInstance();
+   
+        $instance->reset();
+    }
+}
