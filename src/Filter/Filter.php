4e9a7fc576051964f87f94a47f8d27a7279074e7
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-08-01 09:32:46 +0200

    Create Filter.php

---
+++ src/Filter/Filter.php
@@ -0,0 +1,34 @@
+<?php
+// Copyright (c) 2013 - 2017 by Thomas Pawlitschko. All rights reserved.
+
+declare(strict_types=1);
+
+namespace LiTE\Filter;
+
+use LiTE\Assert\Assert;
+
+class Filter implements FilterInterface
+{
+    /**
+     * @param string $name
+     * @return bool
+     */
+    public function isValidName(string $name): bool
+    {
+        $nameLength = strlen($name);
+
+        Assert::notEqualsZero($nameLength);
+
+        $ascii = ord($name);
+
+        if (!($ascii >= 97 && $ascii <= 122 || // a - z
+            $ascii >= 65 && $ascii <= 90 || // A - Z
+            $ascii === 95)) { // _
+
+            return false;
+        }
+        return strspn($name,
+            'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_',
+            1) === $nameLength - 1;
+    }
+}
