1c7ea578c3e4082d17263592b2059095f4d50c59
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-08-01 11:25:10 +0200

    Create VariablesContext.php

---
+++ src/Context/VariablesContext.php
@@ -0,0 +1,47 @@
+<?php
+// Copyright (c) 2013 - 2017 by Thomas Pawlitschko. All rights reserved.
+
+declare(strict_types=1);
+
+namespace LiTE\Context;
+
+use LiTE\Filter\FilterInterface;
+use LiTE\Exceptions\VariablesContextException;
+
+class VariablesContext
+{
+    /**
+     * @var array
+     */
+    private $variables;
+
+    /**
+     * @param array $variables
+     * @return void
+     */
+    public function __construct(array $variables)
+    {
+        $this->variables = $variables;
+    }
+
+    /**
+     * @param string $name
+     * @return mixed
+     */
+    public function lookUp(string $name, FilterInterface $filter)
+    {
+        if (!$filter->isValidName($name)) {
+
+            throw new VariablesContextException(
+                "Invalid variable name: {$name}");
+        }
+        $isVariableExisting = array_key_exists($name, $this->variables);
+
+        if (!$isVariableExisting) {
+
+            throw new VariablesContextException(
+                "Template variable {$name} does not exist");
+        }
+        return $this->variables[$name];
+    }
+}
