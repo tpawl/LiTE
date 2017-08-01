25d4ef3d5331b21d64ec357794a2b103da766a9c
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-08-01 16:28:31 +0200

    Create ViewHelperContextException.php

---
+++ src/Exceptions/ViewHelperContextException.php
@@ -0,0 +1,10 @@
+<?php
+// Copyright (c) 2015 - 2017 by Thomas Pawlitschko. All rights reserved.
+
+declare(strict_types=1);
+
+namespace LiTE\Exceptions;
+
+class ViewHelperContextException extends \LogicException
+{
+}
