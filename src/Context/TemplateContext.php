4e94eeee895343928c2f1d05d76fd2ccd4ece03c
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-08-01 20:37:52 +0200

    Create TemplateContext.php

---
+++ src/Context/TemplateContext.php
@@ -0,0 +1,39 @@
+<?php
+// Copyright (c) 2013 - 2017 by Thomas Pawlitschko. All rights reserved.
+
+declare(strict_types=1);
+
+namespace LiTE\Context;
+
+class TemplateContext
+{
+   /**
+    * @var string
+    */
+    private $template = null;
+
+    /**
+     * @param string $template
+     * @return void
+     */
+    public function __construct(string $template)
+    {
+        $this->template = $template;
+    }
+
+    /**
+     * @return string
+     */
+    public function get(): string
+    {
+        return $this->template;
+    }
+
+    /**
+     * @return void
+     */
+    public function clear(): void
+    {
+        $this->template = null;
+    }
+}
