8fe16960cd9db022b5926bae7533afbbfdf55651
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-08-01 19:52:19 +0200

    Create ViewHelperException.php

---
+++ src/Exceptions/ViewHelperException.php
@@ -0,0 +1,10 @@
+<?php
+// Copyright (c) 2016, 2017 by Thomas Pawlitschko. All rights reserved.
+
+declare(strict_types=1);
+
+namespace LiTE\Exceptions;
+
+class ViewHelperException extends \RuntimeException
+{
+}
