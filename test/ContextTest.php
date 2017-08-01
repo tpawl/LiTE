942193a305168310133869f81b8da5c790252ba8
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-08-01 23:30:00 +0200

    Create ContextTest.php

---
+++ test/ContextTest.php
@@ -0,0 +1,37 @@
+<?php
+// Copyright (c) 2017 by Thomas Pawlitschko. All rights reserved.
+
+declare(strict_types=1);
+
+namespace LiTE\Tests;
+
+use PHPUnit\Framework\TestCase;
+use LiTE\Context\Context;
+
+class ContextTest extends TestCase
+{
+    public function testPushVariablesContext()
+    {
+        $instance = Context::getInstance();
+        
+        $vc1 = new VariablesContext([]);
+        
+        $instance->pushVariablesContext($vc1);
+        
+        $vc2 = new VariablesContext([]);
+        
+        $instance->pushVariablesContext($vc2);
+        
+        $vc = $instance->topVariablesContext();
+        
+        $this->assertIsSame($vc2, $vc);
+        
+        $instance->popVariablesContext();
+        
+        $vc = $instance->topVariablesContext();
+        
+        $this->assertIsSame($vc1, $vc);
+        
+        $instance->popVariablesContext();
+    }
+}
