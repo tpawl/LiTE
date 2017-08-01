f7ac772c5fde3bf03b529a07277bd356a08c340d
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-08-01 09:26:23 +0200

    Create FilterTest.php

---
+++ test/FilterTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Felix\Packages\LiTE\Tests;
+
+require_once __DIR__ . '/../src/Filter/FilterInterface.php';
+require_once __DIR__ . '/../src/Filter/Filter.php';
+require_once __DIR__ . '/../src/Assert/Assert.php';
+require_once __DIR__ . '/../src/Exceptions/AssertException.php';
+
+use PHPUnit\Framework\TestCase;
+use Felix\Packages\LiTE\Filter\Filter;
+
+class FilterTest extends TestCase
+{
+    /**
+     * @expectedException Felix\Packages\LiTE\Exceptions\AssertException
+     */
+    public function testEmptyNameThrowsAnException()
+    {
+        $filter = new Filter();
+
+        $filter->isValidName('');
+    }
+
+    public function testFirstInvalidCharacterOfNameReturnsFalse()
+    {
+        $filter = new Filter();
+
+        $isValidName = $filter->isValidName('3');
+
+        $this->assertFalse($isValidName);
+    }
+}
