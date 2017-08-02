d92e505abea4523c4d6843d676829f58372b58f9
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-08-02 04:20:33 +0200

    Create VariableExpressionTest.php

---
+++ test/VariableExpressionTest.php
@@ -0,0 +1,34 @@
+<?php
+// Copyright (c) 2017 by Thomas Pawlitschko. All rights reserved.
+
+declare(strict_types=1);
+
+namespace LiTE\Tests;
+
+use PHPUnit\Framework\TestCase;
+use LiTE\Context\Context;
+use LiTE\Expressions\VariableExpression;
+use LiTE\Expressions\TemplateExpression;
+
+class TemplateExpressionTest extends TestCase
+{
+    public function testDisplay()
+    {
+        $te = new TemplateExpression('', ['aaa' => 'bbb', var' => 'def', 'ccc' => 'ddd'], __DIR__ . '/Asset', '');
+        
+        $filter = $this->createMock(FilterInterface::class);
+        
+        $filter->method('isValidName')->
+            willReturn(true);
+            
+        $ve = new VariableExpression('var', $filter);
+        
+        $this->expectOutputString('def');
+        
+        $ve->display();
+        
+        $instance = Context::getInstance();
+   
+        $instance->reset();
+    }
+}
