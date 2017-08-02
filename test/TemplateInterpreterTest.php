1b4a6b4af54319ed888fc482eafb5739a9564f38
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-08-02 06:47:34 +0200

    Create TemplateInterpreterTest.php

---
+++ test/TemplateInterpreterTest.php
@@ -0,0 +1,28 @@
+<?php
+// Copyright (c) 2017 by Thomas Pawlitschko. All rights reserved.
+
+declare(strict_types=1);
+
+namespace LiTE\Tests;
+
+use PHPUnit\Framework\TestCase;
+use LiTE\Interpreter\TemplateInterpreter;
+use LiTE\Context\TemplateContext();
+use Lite\Context\Context;
+
+class TemplateInterpreterTest extends TestCase
+{
+    $instance = Context::getInstance();
+    
+    $tc = new TemplateContext('');
+    
+    $instance->setTemplateContext($tc);
+    
+    $ti = new TemplateInterpreter();
+    
+    $this->expectedOutputString('<?xml test ?>');
+    
+    $ti->_xml('test');
+    
+    $instance->reset();
+}
