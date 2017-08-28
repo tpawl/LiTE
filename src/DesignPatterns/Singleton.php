f41bf9e9fb68143d449fa8274746b95091d57101
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-08-28 21:11:56 +0200

    Create Singleton.php

---
+++ src/DesignPatterns/Singleton.php
@@ -0,0 +1,43 @@
+<?php
+// Copyright (c) 2013 - 2017 by Thomas Pawlitschko. All rights reserved.
+
+declare(strict_types=1);
+
+namespace LiTE\DesignPatterns;
+
+class Singleton
+{
+    private static $instance = null;
+
+    public static function getInstance()
+    {
+        if (is_null(self::$instance))
+        {
+            self::$instance = new static();
+        }
+        return self::$instance;
+    }
+    
+    /**
+     * @return void
+     */
+    private function __construct()
+    {
+    }
+    
+        /**
+     * @return void
+     * @codeCoverageIgnore
+     */
+    private function __clone()
+    {
+    }
+    
+    /**
+     * @return void
+     * @codeCoverageIgnore
+     */
+    private function __wakeup()
+    {
+    }
+}
