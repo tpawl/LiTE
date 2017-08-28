33c49f92d4a300239ebcf606845aa5d55812a032
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-08-28 18:04:22 +0200

    Update Configuration.php

--- src/Php/Configuration.php
+++ src/Php/Configuration.php
@@ -5,10 +5,44 @@ declare(strict_types=1);
 
 namespace LiTE\Php;
 
-class Configuration
+class Context
 {
-    public static function shouldErrorLevelReport(int $errorLevel): bool
+    private static $instance = null;
+    
+    public static function getInstance()
     {
-        return error_reporting() & $errorLevel;
+        if (is_null(self::$instance))
+        {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+    
+    /**
+     * @return void
+     */
+    private function __construct()
+    {
+    }
+    
+    public function shouldErrorLevelReport(int $errorLevel): bool
+    {
+        return (bool) (error_reporting() & $errorLevel);
+    }
+    
+     /**
+     * @return void
+     * @codeCoverageIgnore
+     */
+    private function __clone()
+    {
+    }
+    
+    /**
+     * @return void
+     * @codeCoverageIgnore
+     */
+    private function __wakeup()
+    {
     }
 }
