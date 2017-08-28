acc2306dd9fee968f7512663780d80a334bc219a
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-08-28 21:41:52 +0200

    Update Configuration.php

--- src/Php/Configuration.php
+++ src/Php/Configuration.php
@@ -8,9 +8,25 @@ namespace LiTE\Php;
 use LiTE\DesignPatterns\Singleton;
 
 class Configuration extends Singleton
-{   
+{ 
+    private static $instance = null;
+
+    public static function getInstance()
+    {
+        if (is_null(self::$instance))
+        {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+    
     public function shouldErrorLevelBeReported(int $errorLevel): bool
     {
         return (bool) (error_reporting() & $errorLevel);
+    }
+    
+    public function setInstance($instance): void
+    {
+        self::$instance = $instance;
     }
 }
