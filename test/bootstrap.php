23f566be742c3a5f4c1558b725580996f2c4f9f3
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-12-10 19:25:32 +0100

    Update bootstrap.php

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -6,6 +6,7 @@ function loader($class)
     $file = __DIR__ . '/../src/' . str_replace('\\', '/', substr($class, 11)) . '.php';
     
     if (file_exists($file)) {
+        
         require $file;
     }
 }
