ce26202af2e24f808e64a197e3d7c31b61aeb52f
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-12-11 09:22:42 +0100

    Create TestViewHelper.php

---
+++ test/Asset/ViewHelpers/TestViewHelper.php
@@ -0,0 +1,12 @@
+<?php
+// Copyright (c) 2017 by Thomas Pawlitschko. (MIT License)
+
+use tpawl\lite\ViewHelperInterface;
+
+class TestViewHelper implements ViewHelperInterface
+{
+    public static function execute(array $arguments): void
+    {
+        print 'abc';
+    }
+}
