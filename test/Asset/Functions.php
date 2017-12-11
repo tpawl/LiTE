b4ad128691c559aed91784d52df65edec9c30d22
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-12-11 07:20:15 +0100

    Update Functions.php

--- test/Asset/Functions.php
+++ test/Asset/Functions.php
@@ -1,1 +1,13 @@
+<?php
+// Copyright (c) 2017 by Thomas Pawlitschko. (MIT License)
 
+declare(strict_types=1);
+
+namespace tpawl\lite\Tests\Asset;
+
+class Functions
+{
+    public static resetContext(): void
+    {
+    }
+}
