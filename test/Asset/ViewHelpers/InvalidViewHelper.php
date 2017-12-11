dec5d554e21357396795f28ac72daf7cee184ef7
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-12-11 09:20:47 +0100

    Create InvalidViewHelper.php

---
+++ test/Asset/ViewHelpers/InvalidViewHelper.php
@@ -0,0 +1,10 @@
+<?php
+// Copyright (c) 2017 by Thomas Pawlitschko. (MIT License)
+
+class InvalidViewHelper
+{
+    public static function execute(array $arguments): void
+    {
+        print 'abc';
+    }
+}
