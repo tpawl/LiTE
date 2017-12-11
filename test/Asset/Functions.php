c2e22744340b1cae87e02c1b133e73132dbc89ef
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-12-11 07:22:53 +0100

    Update Functions.php

--- test/Asset/Functions.php
+++ test/Asset/Functions.php
@@ -5,9 +5,16 @@ declare(strict_types=1);
 
 namespace tpawl\lite\Tests\Asset;
 
+use tpawl\lite\Context\Context;
+
 class Functions
 {
     public static resetContext(): void
     {
+        $ref = new \ReflectionProperty(Context::class, 'instance');
+        
+        $ref->setAccessible(true);
+        
+        $ref->setValue(null);
     }
 }
