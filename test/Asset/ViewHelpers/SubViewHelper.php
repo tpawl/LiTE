b1af3f16736bde2f5ecf71c10df01f91fbe180cf
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-12-11 09:21:49 +0100

    Create SubViewHelper.php

---
+++ test/Asset/ViewHelpers/SubViewHelper.php
@@ -0,0 +1,15 @@
+<?php
+// Copyright (c) 2017 by Thomas Pawlitschko. (MIT License)
+
+use tpawl\lite\ViewHelperInterface;
+use tpawl\lite\Expressions\SubTemplateExpression;
+
+class SubViewHelper implements ViewHelperInterface
+{
+    public static function execute(array $arguments): void
+    {
+        $ste = new SubTemplateExpression('abc<?php $this->var; ?><?php self::test(); ?>', ['var' => 'def']);
+        
+        $ste->display();
+    }
+}
