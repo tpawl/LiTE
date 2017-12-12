cde7010928b3baf43d92a5ab0c22cd0233e415b6
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2017-12-12 22:18:30 +0100

    Update WrongViewHelper.php

--- tests/Asset/ViewHelpers/WrongViewHelper.php
+++ tests/Asset/ViewHelpers/WrongViewHelper.php
@@ -1,9 +1,9 @@
 <?php
 // Copyright (c) 2017 by Thomas Pawlitschko. (MIT License)
 
-use tpawl\lite\ViewHelperInterface;
+use TPawl\LiTE\ViewHelperInterface;
 
-class xxxViewHelper implements ViewHelperInterface
+class XxxViewHelper implements ViewHelperInterface
 {
     public static function execute(array $arguments): void
     {
