0650becf93dfae2ff6a8f8da5d79a907f88235bb
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2018-03-31 05:32:24 +0200

    Update TemplateExpressionTest.php

--- tests/TemplateExpressionTest.php
+++ tests/TemplateExpressionTest.php
@@ -140,4 +140,87 @@ class TemplateExpressionTest extends TestCase
    
         Functions::resetContext();
     }
+    
+    class ViewHelperContextTest extends TestCase
+{
+    /**
+     * @expectedException \DomainException
+     * @expectedExceptionMessage Invalid view helper name: abc
+     */
+    public function testInvalidNameThrowsAnException()
+    {
+        $viewHelperContext = new ViewHelperContext('', '', null);
+
+        $filter = $this->createMock(FilterInterface::class);
+
+        $filter->method('isValidName')->
+            willReturn(false);
+
+        $viewHelperContext->execute('abc', [], $filter);
+    }
+
+    public function testNormalOperation()
+    {
+        $viewHelperContext = new ViewHelperContext(__DIR__ . '/Asset/ViewHelpers', '', null);
+
+        $filter = $this->createMock(FilterInterface::class);
+
+        $filter->method('isValidName')->
+            willReturn(true);
+
+        $viewHelperContext->execute('test', [], $filter);
+
+        $out = $this->getActualOutput();
+
+        $this->assertEquals('ghi', $out);
+
+        $this->expectOutputString('ghighi');
+
+        $viewHelperContext->execute('test', [], $filter);
+    }
+
+    /**
+     * @expectedException TPawl\LiTE\Exceptions\ViewHelperContextException
+     */
+    public function testWrongViewHelperThrowsAnException()
+    {
+        $viewHelperContext = new ViewHelperContext(__DIR__ . '/Asset/ViewHelpers', '', null);
+
+        $filter = $this->createMock(FilterInterface::class);
+
+        $filter->method('isValidName')->
+            willReturn(true);
+
+        $viewHelperContext->execute('wrong', [], $filter);
+    }
+
+    /**
+     * @expectedException TPawl\LiTE\Exceptions\ViewHelperContextException
+     */
+    public function testInvalidViewHelperThrowsAnException()
+    {
+        $viewHelperContext = new ViewHelperContext(__DIR__ . '/Asset/ViewHelpers', '', null);
+
+        $filter = $this->createMock(FilterInterface::class);
+
+        $filter->method('isValidName')->
+            willReturn(true);
+
+        $viewHelperContext->execute('invalid', [], $filter);
+    }
+
+    /**
+     * @expectedException TPawl\LiTE\Exceptions\ViewHelperException
+     */
+    public function testEceptionViewHelperThrowsAnException()
+    {
+        $viewHelperContext = new ViewHelperContext(__DIR__ . '/Asset/ViewHelpers', '', null);
+
+        $filter = $this->createMock(FilterInterface::class);
+
+        $filter->method('isValidName')->
+            willReturn(true);
+
+        $viewHelperContext->execute('exception', [], $filter);
+    }
 }
