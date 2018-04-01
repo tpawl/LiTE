5441ae306531321d94f6759c1568d81f9dece20f
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2018-04-01 20:44:25 +0200

    Update Version.php

--- src/Version.php
+++ src/Version.php
@@ -8,11 +8,11 @@ namespace TPawl\LiTE;
 class Version
 {
     // Major version number (major release)
-    public const MAJOR = 1;
+    public const MAJOR = 2;
 
     // Minor version number (minor release)
     public const MINOR = 0;
 
     // Revision number (patch level)
-    public const REVISION = 3;
+    public const REVISION = 0;
 }
