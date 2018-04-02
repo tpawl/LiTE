da78938ec951413666074c2faeebded3992dc355
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2018-04-02 08:45:58 +0200

    Update ContextTest.php

--- tests/ContextTest.php
+++ tests/ContextTest.php
@@ -13,6 +13,11 @@ use TPawl\LiTE\Tests\Asset\Functions;
 
 class ContextTest extends TestCase
 {
+    protected function tearDown()
+    {
+        Functions::resetContext();
+    }
+
     /**
      * @expectedException TPawl\LiTE\Exceptions\ContextException
      */
