cfc09d5bf9cda8119875acfd178e6d667ea1c1f9
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2018-04-18 14:48:10 +0200

    Create AssertionTest.php

---
+++ tests/AssertionTest.php
@@ -0,0 +1,21 @@
+<?php
+// Copyright (c) 2017, 2018 by Thomas Pawlitschko. (MIT License)
+
+declare(strict_types=1);
+
+namespace tpawl\lite\Tests;
+
+use PHPUnit\Framework\TestCase;
+use TPawl\LiTE\Assertion\Assertion;
+
+class AssertionTest extends TestCase
+{
+    /**
+     * @expectedException TPawl\LiTE\Exceptions\AssertException
+     * @expectedExceptionMessage abc
+     */
+    public function testTrueConditionThrowsAnException()
+    {
+        Assertion::isFalse(true, 'abc');
+    }
+}
