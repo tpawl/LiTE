60be317c43dbad28ab9057ff3a7259d3c63a683b
Thomas Pawlitschko <Thomas Pawlitschko@Elses-PC>
2018-04-21 14:53:02 +0200

    Update

--- src/Php/Configuration.php
+++ src/Php/Configuration.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright (c) 2013 - 2017 by Thomas Pawlitschko. (MIT License)
+// Copyright (c) 2013 - 2018 by Thomas Pawlitschko. (MIT License)
 
 declare(strict_types=1);
 
@@ -7,6 +7,10 @@ namespace TPawl\LiTE\Php;
 
 class Configuration implements ConfigurationInterface
 {
+    /**
+     * @param int $errorLevel
+     * @return bool
+     */
     public function shouldErrorLevelBeReported(int $errorLevel): bool
     {
         return (bool) (error_reporting() & $errorLevel);
