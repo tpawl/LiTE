3cdc30ce6f069d3f47f76c72bb371b603e109656
Thomas Pawlitschko <Thomas Pawlitschko@Elses-PC>
2018-04-25 06:21:56 +0200

    Update

--- src/Filter/FilterInterface.php
+++ src/Filter/FilterInterface.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright (c) 2013 - 2017 by Thomas Pawlitschko. (MIT License)
+// Copyright (c) 2013 - 2018 by Thomas Pawlitschko. (MIT License)
 
 declare(strict_types=1);
 
@@ -10,6 +10,7 @@ interface FilterInterface
     /**
      * @param string $name
      * @return bool
+     * @throws \TPawl\LiTE\Exceptions\AssertionException if name is empty
      */
     public function isValidName(string $name): bool;
 }
