9c210b2cb66b9e0e697f66beab98c7e38f0c9345
Thomas Pawlitschko <Thomas Pawlitschko@Elses-PC>
2018-04-25 22:57:47 +0200

    Created

---
+++ src/Miscellaneous/ViewHelperCallData.php
@@ -0,0 +1,47 @@
+<?php
+// Copyright (c) 2018 by Thomas Pawlitschko. (MIT License)
+
+declare(strict_types=1);
+
+namespace TPawl\LiTE\Miscellaneous;
+
+class ViewHelperCallData
+{
+    /**
+     * @var string
+     */
+    private $name;
+    
+    /**
+     * @var array
+     */
+    private $arguments;
+    
+    /**
+     * @param string $name
+     * @param array $arguments
+     * @return void
+     */
+    public function __construct(string $name, array $arguments)
+    {
+        $this->name = $name;
+        
+        $this->arguments = $arguments;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    
+    /**
+     * @return array
+     */
+    public function getArguments(): array
+    {
+        return $this->arguments;
+    }
+}
