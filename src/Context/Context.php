aa1ea49e8d784cf5019e43e1b3b5d415894bfe55
Thomas Pawlitschko <Thomas Pawlitschko@Elses-PC>
2018-04-28 13:25:28 +0200

    Update

--- src/Context/Context.php
+++ src/Context/Context.php
@@ -70,6 +70,9 @@ class Context
      * @param string $name
      * @param \TPawl\LiTE\Filter\FilterInterface $filter
      * @return mixed
+     * @throws \TPawl\LiTE\Exceptions\AssertionException if name is empty
+     * @throws \DomainException if name is invalid
+     * @throws \OutOfRangeException if name does not exist
      */
     public function lookUpVariable(string $name, FilterInterface $filter)
     {
