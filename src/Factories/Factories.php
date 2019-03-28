cbdfd5453c7ef0ad2724b63bbcd7235e159b4d0f
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2019-03-28 07:40:59 +0100

    Create Factories.php

---
+++ src/Factories/Factories.php
@@ -0,0 +1,20 @@
+<?php
+// Copyright (c) 2019 by Thomas Pawlitschko. (MIT License)
+
+declare(strict_types=1);
+
+namespace TPawl\LiTE\Factories;
+
+use TPawl\LiTE\Filter\FilterInterface;
+use TPawl\LiTE\Filter\Filter;
+
+class FilterFactory
+{
+    /**
+     * @return \TPawl\LiTE\Filter\FilterInterface
+     */
+    public static function createFilter(): FilterInterface
+    {
+        return new Filter();
+    }
+}
