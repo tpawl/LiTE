c413f98269cddc5d3562a8393c5ced8a23743942
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2019-03-28 07:49:39 +0100

    Update Factories.php

--- src/Factories/Factories.php
+++ src/Factories/Factories.php
@@ -6,10 +6,18 @@ declare(strict_types=1);
 namespace TPawl\LiTE\Factories;
 
 use TPawl\LiTE\Filter\FilterInterface;
+use TPawl\LiTE\Expressions\TemplateExpressionInterface;
+use TPAwl\LiTE\Expressions\VariableExpression;
 use TPawl\LiTE\Filter\Filter;
 
 class Factories
 {
+    public static function createVariableExpression(
+        string $name, FilterInterface $filter): TemplateExpressionInterface
+    {
+        return new VariableExpression($name, $filter);
+    }
+    
     /**
      * @return \TPawl\LiTE\Filter\FilterInterface
      */
