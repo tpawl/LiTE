0804da5eac8b8ce95459ad31e57559d125fa3745
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2019-03-28 02:30:17 +0100

    Update FilterFactory.php

--- src/Factories/FilterFactory.php
+++ src/Factories/FilterFactory.php
@@ -1,1 +1,16 @@
+<?php
+// Copyright (c) 2019 by Thomas Pawlitschko. (MIT License)
 
+declare(strict_types=1);
+
+namespace TPawl\LiTE\Factories;
+
+use TPawl\LiTE\Filter\FilterInterface;
+
+class FilterFactory
+{
+    public static function createFilter(): FilterInterface
+    {
+        return new Filter();
+    }
+}
