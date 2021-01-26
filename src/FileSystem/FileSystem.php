3d829f7809659666674acf91ee8881ec2502dfc2
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2021-01-26 21:42:29 +0100

    Create FileSystem.php

---
+++ src/FileSystem/FileSystem.php
@@ -0,0 +1,35 @@
+<?php
+// Copyright (c) 2021 by Thomas Pawlitschko. (MIT License)
+
+declare(strict_types=1);
+
+namespace TPawl\LiTE\FileSystem;
+
+use TPawl\LiTE\Exceptions\FileSystemException;
+
+class FileSystem
+{
+    /**
+     * @return void
+     * @codeCoverageIgnore
+     */
+    private function __construct()
+    {
+    }
+
+    /*
+	   * @param string $pathname
+	   * @return void
+	   */
+    public static function makeRealPathname(string $pathname): string
+	  {
+		    $realPathname = realpath($pathname);
+		
+		    if ($realPathname === false) {
+			
+			      throw new FileSystemException(
+			          "can not make real pathname for: '{$pathname}'");
+	      }
+		    return $realPathname;
+	  }
+}
