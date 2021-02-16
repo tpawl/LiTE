c22053cc7e68c06876dc9ceab17d7e063b9b2a1b
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2021-02-16 20:45:00 +0100

    Update StringType.php

--- src/String/StringType.php
+++ src/String/StringType.php
@@ -1,1 +1,17 @@
-<?php>
+<?php
+// Copyright (c) 2021 by Thomas Pawlitschko. (MIT License)
+
+declare(strict_types=1);
+
+namespace TPawl\LiTE\String;
+
+class StringType
+{
+    public const EMPTY_STRING = '';
+    
+    public static function isEmptyString(string $string): bool
+    {
+        return empty($string);
+    }
+}
+    
