fa26331c967f756786bb5f80a04df02ae281d4f9
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2021-02-16 21:06:11 +0100

    Create StringTypeTest.php

---
+++ tests/StringTypeTest.php
@@ -0,0 +1,27 @@
+<?php
+// Copyright (c) 2021 by Thomas Pawlitschko. (MIT License)
+
+declare(strict_types=1);
+
+namespace tpawl\lite\Tests;
+
+use PHPUnit\Framework\TestCase;
+use TPawl\LiTE\String\StringType;
+
+class StringTypeTest extends TestCase
+{   
+    public function testIsEmptyStringReturnsTrue()
+    {
+        $this->assertTrue(StringType::isEmptyString(''));
+    }
+    
+    public function testIsEmptyStringReturnsFalse()
+    {   
+        $this->assertFalse(StringType::isEmptyString('abc'));
+    }
+    
+    public function testEmptyString()
+    {
+        $this->assertTrue(StringType::isEmptyString(StringType::EMPTY_STRING));
+    }
+}
