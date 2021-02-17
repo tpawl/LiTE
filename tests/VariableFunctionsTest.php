8e098834709455d3787cb120ddae1ac073e9e9ce
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2021-02-17 21:04:00 +0100

    Update VariableFunctionsTest.php

--- tests/VariableFunctionsTest.php
+++ tests/VariableFunctionsTest.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
 namespace tpawl\lite\Tests;
 
 use PHPUnit\Framework\TestCase;
-use TPawl\LiTE\Variable\VariableFunctions;
+use TPawl\LiTE\Miscellaneous\VariableFunctions;
 
 class VariableFunctionsTest extends TestCase
 {
