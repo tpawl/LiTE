f6e5b96ef7d033f4ff1201966bc7c02e32ce1cb8
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2021-02-17 03:20:53 +0100

    Update Assertion.php

--- src/Assertion/Assertion.php
+++ src/Assertion/Assertion.php
@@ -9,15 +9,16 @@ use TPawl\LiTE\Exceptions\AssertionException;
 use TPawl\LiTE\String\StringType;
 
 class Assertion
-{   
+{
     /**
      * @param mixed $value
      * @throws \TPawl\LiTE\Exceptions\AssertionException if value is null
      */
-    public static function assertNotIsNull($value, string $message = StringType::EMPTY_STRING): void
+    public static function assertNotIsNull(
+        $value, string $message = StringType::EMPTY_STRING): void
     {
         if (is_null($value)) {
-            
+
             self::assertNeverReachesHere($message);
         }
     }
@@ -26,18 +27,19 @@ class Assertion
      * @throws \TPawl\LiTE\Exceptions\AssertionException if string is empty
      */
     public static function assertNotIsEmptyString(
-	    string $string, string $message = StringType::EMPTY_STRING): void
+        string $string, string $message = StringType::EMPTY_STRING): void
     {
         if (StringType::isEmptyString($string)) {
-            
+
             self::assertNeverReachesHere($message);
         }
     }
-    
+
     /**
      * @throws \TPawl\LiTE\Exceptions\AssertionException anyway
      */
-    public static function assertNeverReachesHere(string $message = StringType::EMPTY_STRING): void
+    public static function assertNeverReachesHere(
+        string $message = StringType::EMPTY_STRING): void
     {
         throw new AssertionException($message);
     }
