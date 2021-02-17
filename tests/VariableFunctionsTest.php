2e5541871bcb06345924856d0add9c6ae2ef2d9a
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2021-02-17 03:48:03 +0100

    Create VariableFunctionsTest.php

---
+++ tests/VariableFunctionsTest.php
@@ -0,0 +1,22 @@
+<?php
+// Copyright (c) 2021 by Thomas Pawlitschko. (MIT License)
+
+declare(strict_types=1);
+
+namespace tpawl\lite\Tests;
+
+use PHPUnit\Framework\TestCase;
+use TPawl\LiTE\String\VariableFunctions;
+
+class VariableFunctionsTest extends TestCase
+{   
+    public function testIsNullReturnsTrue()
+    {
+        $this->assertTrue(VariableFunctions::isNull(null));
+    }
+    
+    public function testIsNullReturnsFalse()
+    {   
+        $this->assertFalse(VariableFunctions::isNull('not null'));
+    }
+}
