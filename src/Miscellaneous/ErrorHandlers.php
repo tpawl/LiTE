5f2a330a8c099fdfc7ea3d634526c89a4ddd1a11
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2021-02-25 05:33:15 +0100

    Create ErrorHandlers.php

---
+++ src/Miscellaneous/ErrorHandlers.php
@@ -0,0 +1,46 @@
+<?php
+// Copyright (c) 2015 - 2017 by Thomas Pawlitschko. (MIT License)
+
+declare(strict_types=1);
+
+namespace TPawl\LiTE\Miscellaneous;
+
+class ErrorHandlers
+{
+    /**
+     * @return void
+     * @codeCoverageIgnore
+     */
+    private function __construct()
+    {
+    }
+
+    /**
+     * @param callable|null $errorHandler
+     * @return void
+     */
+    public static function push(?callable $errorHandler): void
+    {
+        set_error_handler($errorHandler);
+    }
+
+    /**
+     * @return callable|null
+     */
+    public static function top(): ?callable
+    {
+        $previousErrorHandler = set_error_handler(null);
+
+        self::pop();
+
+        return $previousErrorHandler;
+    }
+
+    /**
+     * @return void
+     */
+    public static function pop(): void
+    {
+        restore_error_handler();
+    }
+}
