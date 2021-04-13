f39cc339e2397f522eb92a6019b88550070215bd
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2021-04-13 09:17:52 +0200

    Update PackageInformations.php

--- src/PackageInformations.php
+++ src/PackageInformations.php
@@ -8,28 +8,42 @@ namespace TPawl\LiTE;
 class PackageInformations
 {
     private const SEMICOLON_SPACE = ': ';
-
+    private const DOT = '.';
+    private const COMMA_SPACE = ', ';
+    
     public const PACKAGE_NAME = 'LiTE';
 
     // Major version number (major release)
     public const MAJOR_VERSION = 2;
-
     // Minor version number (minor release)
     public const MINOR_VERSION = 0;
-
     // Revision number (patch level)
     public const REVISION_NUMBER = 0;
 
-    public const AUTHORS = ['Thomas Pawlitschko'];
+    public const PACKAGE_AUTHORS = ['Thomas Pawlitschko'];
 
     public const COPYRIGHT_YEARS = '2013 - 2021';
-
     public const COPYRIGHT_HOLDERS = ['Thomas Pawlitschko'];
 
-    public const LICENSE = 'MIT License';
+    public const PACKAGE_LICENSE = 'MIT License';
 
     public static function prependPackageName(string $string): string
     {
         return self::PACKAGE_NAME . self::SEMICOLON_SPACE . $string;
+    }
+    
+    public static function makeVersionString(): string
+    {
+        return self::VERSION_MAJOR . self::DOT . self::VERSION_MINOR . self::DOT . self::REVISION_NUMBER;
+    }
+    
+    public static function makePackageAuthorsString(): string
+    {
+        return implode(self::COMMA_SPACE, self::PACKAGE_AUTHORS);
+    }
+    
+    public static function makeCopyrightHoldersString(): string
+    {
+        return implode(self::COMMA_SPACE, self::COPYRIGHT_HOLDERS);
     }
 }
