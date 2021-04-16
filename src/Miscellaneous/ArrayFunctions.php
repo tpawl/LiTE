a1e8fa857c226c25fc38f479e5f229c0a7dac4ca
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2021-04-16 20:40:51 +0200

    Update ArrayFunctions.php

--- src/Miscellaneous/ArrayFunctions.php
+++ src/Miscellaneous/ArrayFunctions.php
@@ -1,1 +1,14 @@
+<?php
+// Copyright (c) 2021 by Thomas Pawlitschko. (MIT License)
 
+declare(strict_types=1);
+
+namespace TPawl\LiTE\Miscellaneous;
+
+class ArrayFunctions
+{
+    public static function implodeWith(array $array, string $separator = StringType::EMPTY_STRING): bool
+    {
+        return implode($separator, $array);
+    }
+}
