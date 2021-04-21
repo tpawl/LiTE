06e7d0932bb4aab206395563918f18ec82d084d7
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2021-04-21 04:31:43 +0200

    Update PackageInformationsTest.php

--- tests/PackageInformationsTest.php
+++ tests/PackageInformationsTest.php
@@ -12,31 +12,39 @@ class PackageInformationsTest extends TestCase
 {
     public function testPackageName()
     {
-        $this->assertEquals(PackageInformations::prependPackageName('abc'), 'LiTE: abc');
+        $this->assertEquals(
+            PackageInformations::prependPackageName('abc'), 'LiTE: abc');
     }
 
     public function testPackageVersion()
     {
-        $this->assertEquals(PackageInformations::makePackageVersionString(), '2.0.0');
+        $this->assertEquals(
+            PackageInformations::makePackageVersionString(), '2.0.0');
     }
-    
+
     public function testPackageAuthors()
     {
-        $this->assertEquals(PackageInformations::makePackageAuthorsString(), 'Thomas Pawlitschko');
+        $this->assertEquals(
+            PackageInformations::makePackageAuthorsString(),
+            'Thomas Pawlitschko');
     }
-    
+
     public function testCopyrightYears()
     {
-        $this->assertEquals(PackageInformations::COPYRIGHT_YEARS, '2013 - 2021');
+        $this->assertEquals(
+            PackageInformations::COPYRIGHT_YEARS, '2013 - 2021');
     }
-    
+
     public function testCopyrightHolders()
     {
-        $this->assertEquals(PackageInformations::makeCopyrightHoldersString(), 'Thomas Pawlitschko');
+        $this->assertEquals(
+            PackageInformations::makeCopyrightHoldersString(),
+            'Thomas Pawlitschko');
     }
-    
+
     public function testPackageLicense()
     {
-        $this->assertEquals(PackageInformations::PACKAGE_LICENSE, 'MIT License');
+        $this->assertEquals(
+            PackageInformations::PACKAGE_LICENSE, 'MIT License');
     }
 }
