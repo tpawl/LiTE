6fe79886c83ab9ab3d939271ec78817c082344fb
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2021-04-25 03:10:25 +0200

    Update Loader.php

--- src/Miscellaneous/Loader.php
+++ src/Miscellaneous/Loader.php
@@ -1,1 +1,23 @@
+<?php
+// Copyright (c) 2021 by Thomas Pawlitschko. (MIT License)
 
+declare(strict_types=1);
+
+namespace TPawl\LiTE\Miscellaneous;
+
+use TPawl\LiTE\Exceptions\FileSystemException;
+
+class FileSystem
+{
+    public static function makeRealPathname(string $pathname): string
+    {
+        $realPathname = realpath($pathname);
+
+        if ($realPathname === false) {
+
+            throw new FileSystemException(
+                "Could not make real pathname for '{$pathname}'.");
+        }
+        return $realPathname;
+    }
+}
