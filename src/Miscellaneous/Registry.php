959ec9efefc1254125ffebbcd4915d880fc79743
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2021-06-12 13:42:03 +0200

    Update Registry.php

--- src/Miscellaneous/Registry.php
+++ src/Miscellaneous/Registry.php
@@ -1,1 +1,20 @@
+<?php
+// Copyright (c) 2021 by Thomas Pawlitschko. (MIT License)
 
+declare(strict_types=1);
+
+namespace TPawl\LiTE\Miscellaneous;
+
+class Registry
+{
+    private static instance = null;
+    
+    public static function getInstance(): self
+    {
+        if (self::instance === null) {
+            
+            self::instance = new self();
+        }
+        return self::instance;
+    }
+}
