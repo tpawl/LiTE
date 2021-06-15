6e2aadc12bd2f22fb35c4398cd4c0c0b5aaa7be6
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2021-06-15 09:51:39 +0200

    Create SubTemplate.php

---
+++ src/SubTemplate.php
@@ -0,0 +1,35 @@
+<?php
+// Copyright (c) 2021 by Thomas Pawlitschko. (MIT License)
+
+declare(strict_types=1);
+
+namespace TPawl\LiTE;
+
+use TPawl\LiTE\Expressions\TemplateExpression;
+use Psr\Log\LoggerInterface;
+
+class Template
+{
+    private $templateExpression;
+    
+    /**
+     * @param LoggerInterface|null $securityLogger
+     * @return void
+     * @throws \InvalidArgumentException if an index does not exist or the type at index is wrong
+     */
+    public function __construct(array $settings, ?LoggerInterface $securityLogger = null)
+    {
+        if (!VariableFunctions::isNull($securityLogger)) {
+        
+            $registry = Registry::getInstance();
+        
+            $registry->setSecurityLogger($securityLogger);
+        }
+        $templateExpression = new TemplateExpression($settings);
+    }
+    
+    public function display(): void
+    {
+        $this->templateExpression->display();
+    }
+}
