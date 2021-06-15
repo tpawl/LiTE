f4a9287e4f6c431230d471bb9078983ca027787d
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2021-06-15 08:52:29 +0200

    Update Template.php

--- src/Template.php
+++ src/Template.php
@@ -1,1 +1,28 @@
+<?php
+// Copyright (c) 2021 by Thomas Pawlitschko. (MIT License)
 
+declare(strict_types=1);
+
+namespace TPawl\LiTE;
+
+use TPawl\LiTE\Expressions\TemplateExpression;
+use Psr\Log\LoggerInterface;
+
+class Template
+{
+    /**
+     * @param array $settings
+     * @return void
+     * @throws \InvalidArgumentException if an index does not exist or the type at index is wrong
+     */
+    public function __construct(array $settings, ?LoggerInterface $securityLogger = null)
+    {
+        if (!VariableFunctions::isNull($securityLogger)) {
+        
+            $registry = Registry::getInstance();
+        
+            $registry->setSecurityLogger($securityLogger);
+        }
+        new TemplateExpression($settings);
+    }
+}
