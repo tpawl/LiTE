6efe7565d72297ba4a6be0ff5ca14b378dcfe888
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2021-06-15 14:03:42 +0200

    Update SubTemplateExpressionTest.php

--- tests/SubTemplateExpressionTest.php
+++ tests/SubTemplateExpressionTest.php
@@ -90,19 +90,4 @@ class SubTemplateExpressionTest extends TestCase
 
         Functions::resetContext();
     }
-
-    public function testErrorHandler()
-    {
-		$this->expectException(\ErrorException::class);
-		$this->expectExceptionMessage('Test');
-		
-        $mock = $this->createMock(ConfigurationInterface::class);
-
-        $mock->method('shouldErrorLevelBeReported')->
-            willReturn(true);
-
-        $eh = SubTemplateExpression::getErrorHandler($mock);
-
-        call_user_func($eh, E_WARNING, 'Test', '', 0);
-    }
 }
