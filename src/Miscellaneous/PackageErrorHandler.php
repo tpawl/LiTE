5ad551e2b1946c8cf0f3a5bb118f7e4ab39d7a78
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2021-06-15 13:56:06 +0200

    Create PackageErrorHandler.php

---
+++ src/Miscellaneous/PackageErrorHandler.php
@@ -0,0 +1,38 @@
+<?php
+// Copyright (c) 2021 by Thomas Pawlitschko. (MIT License)
+
+declare(strict_types=1);
+
+namespace TPawl\LiTE\Miscellaneous;
+
+use TPawl\LiTE\Php\Configuration;
+
+class PackageErrorHandler
+{   
+    protected static function pushErrorHandler(): void
+    {
+        $configuration = new Configuration();
+        
+        ErrorHandlersStack::pushErrorHandler(self::getErrorHandler($configuration));
+    }
+    
+    protected static function popErrorHandler(): void
+    {
+        ErrorHandlersStack::popErrorHandler();
+    }
+    
+     /**
+     * @return callable
+     */
+    private static function getErrorHandler(ConfigurationInterface $configuration): callable
+    {
+        return function($errno, $errstr, $errfile, $errline) use ($configuration) {
+
+            if ($configuration->shouldErrorLevelBeReported($errno)) {
+
+                throw new \ErrorException(
+                    $errstr, 0, $errno, $errfile, $errline);
+            }
+        };
+    }
+}
