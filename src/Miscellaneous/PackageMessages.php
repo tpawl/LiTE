27ce891fddfeb6360b4a2be6155cf6b742ec6cfc
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2021-06-20 04:00:28 +0200

    Update PackageMessages.php

--- src/Miscellaneous/PackageMessages.php
+++ src/Miscellaneous/PackageMessages.php
@@ -1,1 +1,18 @@
+<?php
+// Copyright (c) 2013 - 2021 by Thomas Pawlitschko. (MIT License)
 
+declare(strict_types=1);
+
+namespace TPawl\LiTE\Miscellaneous;
+
+use TPawl\LiTE\PackageInformations;
+
+class PackageMessages
+{
+    private const COLON_SPACE = ': ';
+    
+    public static function packagizeMessage(string $message): string
+    {
+        return PackageInformations::PACKAGE_NAME . self::COLON_SPACE . $message;
+    }
+}
