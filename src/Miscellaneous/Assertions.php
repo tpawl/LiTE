f6c53029dcc0cd2a68ed5e21b8247808d6349f6c
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2021-10-18 13:38:46 +0200

    Update Assertions.php

--- src/Miscellaneous/Assertions.php
+++ src/Miscellaneous/Assertions.php
@@ -8,36 +8,37 @@ namespace TPawl\LiTE\Miscellaneous;
 class Assertions
 {   
     /**
-     * @param bool $value
-     * @throws \TPawl\LiTE\Exceptions\AssertionException if value is false
+     * @throws \AssertionError if $boolean is false
      */
     public static function assertTrue(
-        bool $value, string $message = StringType::EMPTY_STRING): void
+        bool $boolean, string $message = StringType::EMPTY_STRING): void
     {
-        assert($value, self::computeMessage($message));
+        assert($boolean, self::computeMessage($message));
     }
 
     /**
      * @param mixed $value
-     * @throws \TPawl\LiTE\Exceptions\AssertionException if value is null
+     * @throws \AssertionError if $value is null
      */
     public static function assertNotNull(
         $value, string $message = StringType::EMPTY_STRING): void
     {
-        assert(!VariableFunctions::isNull($value), self::computeMessage($message));
+        assert(!VariableFunctions::isNull($value),
+            self::computeMessage($message));
     }
 
     /**
-     * @throws \TPawl\LiTE\Exceptions\AssertionException if string is empty
+     * @throws \AssertionError if $string is empty
      */
     public static function assertNotEmptyString(
         string $string, string $message = StringType::EMPTY_STRING): void
     {
-        assert(!StringType::isEmptyString($string), self::computeMessage($message));
+        assert(!StringType::isEmptyString($string),
+            self::computeMessage($message));
     }
 
     /**
-     * @throws \TPawl\LiTE\Exceptions\AssertionException anyway
+     * @throws \AssertionError anyway
      */
     public static function assertNeverReachesHere(
         string $message = StringType::EMPTY_STRING): void
