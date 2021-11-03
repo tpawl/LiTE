9ac7d080ed7e57fabfa4a36816adf3be4174a56c
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2021-11-03 08:50:45 +0100

    Create AsciiCharacterType.php

---
+++ src/Miscellaneous/AsciiCharacterType.php
@@ -0,0 +1,74 @@
+<?php
+// Copyright (c) 2021 by Thomas Pawlitschko. (MIT License)
+
+declare(strict_types=1);
+
+namespace TPawl\LiTE\Miscellaneous;
+
+use TPawl\LiTE\Exceptions\EmptyStringException;
+use TPawl\LiTE\PackageInformations;
+
+class StringType
+{
+    private const ALPHABETIC_NUMERIC_SET =
+        'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    
+    public const EMPTY_STRING = '';
+
+    private const ASCII_LOWER_A = 97;
+    private const ASCII_LOWER_Z = 122;
+    public const ASCII_UNDERSCORE = 95;
+    
+    private const DELTA_UPPER_TO_LOWER_CASE = 32;
+    
+    public static function isEmptyString(string $string): bool
+    {
+        return empty($string);
+    }
+    
+    public static function isAlphabeticNumeric(string $string): bool
+    {
+        return strspn($string, self::ALPHABETIC_NUMERIC_SET) === strlen($string);
+    }
+    
+    public static function convertFirstCharacterToUpperCase(string $string): string
+    {
+        $ascii = ord(string);
+        
+        if (self::isLowerCase($ascii)) {
+        
+            string[0] = chr($ascii - self::DELTA_UPPER_TO_LOWER_CASE);
+        }
+        return string;
+    }
+    
+    public static function isLowerCase(int $ascii): bool
+    {
+        return $ascii >= self::ASCII_LOWER_A && $ascii <= self::ASCII_LOWER_Z;
+    }
+    
+    public static function isBeginningWith(string $string, string $str): bool
+    {
+        $length = strlen($string);
+        $len = strlen($str);
+        
+        if ($length < $len) {
+            
+            return false;
+        }
+        return substr($string, 0, $len) === $str;
+    }
+    
+    public static function getLastCharacter(string $string): string
+    {
+        $stringLength = strlen($string);
+        
+        if ($stringLength === 0) {
+            
+            throw new EmptyStringException(
+                PackageInformations::prependPackageNameColonSpace(
+                    'Try to get character from empty string.'));
+        }
+        return $string[$stringLength - 1];
+    }
+}
