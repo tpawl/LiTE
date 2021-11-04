b515c3e22d11209ec6b64d0db10dd1c31614f759
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2021-11-04 10:08:10 +0100

    Update StringType.php

--- src/Miscellaneous/StringType.php
+++ src/Miscellaneous/StringType.php
@@ -1,18 +1,14 @@
 <?php
-// Copyright (c) 2021 by Thomas Pawlitschko. (MIT License)
+// Copyright (c) 2013 - 2021 by Thomas Pawlitschko. (MIT License)
 
 declare(strict_types=1);
 
 namespace TPawl\LiTE\Miscellaneous;
 
 use TPawl\LiTE\Exceptions\EmptyStringException;
-use TPawl\LiTE\PackageInformations;
 
 class StringType
-{
-    private const ALPHABETIC_NUMERIC_SET =
-        'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    
+{   
     public const EMPTY_STRING = '';
     
     public static function isEmptyString(string $string): bool
@@ -22,16 +18,19 @@ class StringType
     
     public static function isAlphabeticNumeric(string $string): bool
     {
-        return strspn($string, self::ALPHABETIC_NUMERIC_SET) === strlen($string);
+        return strspn($string, AsciiCharacterSets::ALPHABETIC_NUMERIC) ===
+            strlen($string);
     }
     
-    public static function convertFirstCharacterToUppercase(string $string): string
+    public static function
+        convertFirstCharacterToUppercase(string $string): string
     {
         $ascii = ord($string);
         
         if (AsciiCharacterFunctions::isLowercase($ascii)) {
         
-            $string[0] = chr($ascii - AsciiCharacters::DELTA_UPPERCASE_TO_LOWERCASE);
+            $string[0] =
+                chr($ascii - AsciiCharacters::DELTA_UPPERCASE_TO_LOWERCASE);
         }
         return $string;
     }
@@ -55,7 +54,7 @@ class StringType
         if ($stringLength === 0) {
             
             throw new EmptyStringException(
-                PackageInformations::prependPackageNameColonSpace(
+                PackageMessages::packagizeMessage(
                     'Try to get character from empty string.'));
         }
         return $string[$stringLength - 1];
