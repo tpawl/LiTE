2273d3b45833172b5e485e1c9f63bd22aebe9bcd
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2021-11-04 10:08:01 +0100

    Create AsciiCharacterSets.php

---
+++ src/Miscellaneous/AsciiCharacterSets.php
@@ -0,0 +1,12 @@
+<?php
+// Copyright (c) 2013 - 2021 by Thomas Pawlitschko. (MIT License)
+
+declare(strict_types=1);
+
+namespace TPawl\LiTE\Miscellaneous;
+
+class AsciiCharacterSets
+{
+    public const ALPHABETIC_NUMERIC =
+        'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+}
