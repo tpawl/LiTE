2f2f164cddb7c69b60f157f9d2598da9a82bc488
Thomas Pawlitschko <30606552+tpawl@users.noreply.github.com>
2021-11-04 07:56:30 +0100

    Create AsciiCharacters.php

---
+++ src/Miscellaneous/AsciiCharacters.php
@@ -0,0 +1,15 @@
+<?php
+// Copyright (c) 2013 - 2021 by Thomas Pawlitschko. (MIT License)
+
+declare(strict_types=1);
+
+namespace TPawl\LiTE\Miscellaneous;
+
+class AsciiCharacters
+{
+    public const SMALL_A = 97; // ord('a')
+    public const SMALL_Z = 122; // ord('z')    
+    public const UNDERSCORE = 95; // ord('_')
+    
+    public const DELTA_UPPERCASE_TO_LOWERCASE = 32; // i.e. ord('a') - ord('A')
+}
